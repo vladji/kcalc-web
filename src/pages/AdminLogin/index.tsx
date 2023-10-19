206c4c18f232619766942d8972104095e329da58
vladji <vladjislav@gmail.com>
2023-10-19 20:49:24 +0600

    prettier

--- src/pages/AdminLogin/index.tsx
+++ src/pages/AdminLogin/index.tsx
@@ -1,13 +1,13 @@
-import { ChangeEvent, useState } from "react";
-import cn from "classnames";
-import { Button } from "../../components/UI/Button";
-import { useAdminLogin } from "../../requests/useAdminLogin";
-import styles from "./styles.module.scss";
-import { Layout } from "../../components/Layout";
+import { ChangeEvent, useState } from 'react';
+import cn from 'classnames';
+import { Button } from '../../components/UI/Button';
+import { useAdminLogin } from '../../requests/useAdminLogin';
+import { Layout } from '../../components/Layout';
+import styles from './styles.module.scss';
 
 export const AdminLogin = () => {
-  const [name, setName] = useState<string>("");
-  const [pass, setPass] = useState<string>("");
+  const [name, setName] = useState<string>('');
+  const [pass, setPass] = useState<string>('');
 
   const { adminLogin, loading, errorMessage } = useAdminLogin();
 
@@ -28,13 +28,18 @@ export const AdminLogin = () => {
   return (
     <Layout loading={loading}>
       <section className="section-wrapper">
-        <div className={cn("content-wrapper", styles.content)}>
+        <div className={cn('content-wrapper', styles.content)}>
           <div className={styles.titleWrapper}>
             {!errorMessage && <h1>Hello Elya!</h1>}
             {!!errorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
           </div>
           <input className={styles.input} type="text" placeholder="name" onChange={onName} />
-          <input className={styles.input} type="text" placeholder="password" onChange={onPassword} />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="password"
+            onChange={onPassword}
+          />
           <Button handler={onSend}>
             <span>Login</span>
           </Button>
